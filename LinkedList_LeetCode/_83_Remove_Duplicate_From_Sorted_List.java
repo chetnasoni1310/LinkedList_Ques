426feec536aaaf2b7741c689d0c751017713600d
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-01-21 16:42:02 +0530

    updated

---
+++ LinkedList_LeetCode/_83_Remove_Duplicate_From_Sorted_List.java
@@ -0,0 +1,24 @@
+public class _83_Remove_Duplicate_From_Sorted_List {
+      
+    class ListNode 
+    {
+            int val;
+             ListNode next;
+             ListNode() {}
+             ListNode(int val) { this.val = val; }
+             ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+         }
+         public ListNode deleteDuplicates(ListNode head) {
+            ListNode temp=head;
+            while(temp!=null && temp.next!=null ) 
+           { if(temp.val==temp.next.val)
+            {
+                temp.next=temp.next.next;
+            }
+            else{
+                temp=temp.next;
+            }
+           }
+            return head;
+         }
+}
